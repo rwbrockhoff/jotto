e633caf7e9d3d67c5856404ee421eb9c5327207a
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-02 11:50:02 -0600

    setup file configured.

--- src/Congrats/__tests__/Congrats.test.js
+++ src/Congrats/__tests__/Congrats.test.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
 import { findAttr, checkProps } from '../../../test/testUtils'
 import Congrats from '../Congrats'
 
-Enzyme.configure({adapter: new EnzymeAdapter() })
+//-----Set Up------//
 
 const defaultProps = {success: false}
 
@@ -12,6 +11,8 @@ const setup = (props={ success: false }) => {
     const setUpProps = {...defaultProps, ...props}
     return shallow(<Congrats {...setUpProps} />)
 }
+
+//-----Set Up------//
 
 describe('Congrats Component', () => {
 
